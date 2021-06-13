646e5aee128d80494df38da3f19ab052d4defa81
jznxm <keithkun93@gmail.com>
2021-06-13 11:08:38 +0800

    Update caller.c

--- chapter_9/env_test/caller.c
+++ chapter_9/env_test/caller.c
@@ -1,7 +1,9 @@
+#include <stdio.h>
 #include <unistd.h>
 
 int main()
 {
     char *my_env[] = {(char *)"JUICE=peach and apple", NULL};
     execle("diner_info.out", "diner_info.out", "4", NULL, my_env);
+    puts("Hey, there must be somthing wrong.");
 }
