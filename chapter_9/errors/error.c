e3a56f0981d2c4416f4950505b3f6b354a5ad8ba
jznxm <keithkun93@gmail.com>
2021-06-13 10:46:35 +0800

    Update error.c

--- chapter_9/errors/error.c
+++ chapter_9/errors/error.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 int main()
 {
     char error_number[10];
-    while(1)
+    while (1)
     {
         puts("\nPlease, enter a error number:");
         if (fgets(error_number, sizeof(error_number), stdin) != NULL)
@@ -18,4 +19,3 @@ int main()
     }
     return 0;
 }
-
